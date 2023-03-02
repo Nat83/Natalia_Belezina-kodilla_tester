05d2e132b3409df22ddb53e06d1754ea044fec12
Natalia <betalka@yahoo.com>
2023-03-02 20:11:17 +0100

    adding lower boundary value to 'Leap year' method

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,17 +1,20 @@
-
 public class LeapYear {
 
     public static void main(String[] args) {
-        isLeapYear(1009);
+        isLeapYear(1423433436);
     }
 
     private static boolean isLeapYear(int year) {
-        if (year % 4 == 0 & year % 100 != 0){
+        if (year < -238) {
+            System.out.println("The concept of a leap year did not exist yet!");
+            return false;
+        } else if (year % 4 == 0 & year % 100 != 0) {
             System.out.println("It is leap year");
-            return true;}
-        else {
+            return true;
+        } else {
             System.out.println("It is normal year");
-            return false;}
+            return false;
+        }
     }
 }
 

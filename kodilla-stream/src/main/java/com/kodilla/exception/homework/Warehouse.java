853b56570d200645a9aeb6b1e16c02c88e7501f6
Natalia <betalka@yahoo.com>
2023-04-13 17:48:41 +0200

    Moduł 6 - 6.5 Zadanie: obsługa błędów

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -1,28 +1,30 @@
 package com.kodilla.exception.homework;
 
-import com.kodilla.exception.AirportNotFoundException;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Warehouse {
-    List<Order> orders;
+    private List<Order> orders;
 
-    public void addOrder(Order order){
-        orders.add(order);
-
+    public Warehouse() {
+        this.orders = new ArrayList<>();
     }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+        System.out.println(Arrays.toString(orders.toArray()));
+    }
+
     public Order getOrder(String number) throws OrderDoesntExistException {
 
+        orders.stream().filter(order -> order.getNumber().equals(number)).collect(Collectors.toList()).get(0);
 
-        Order searchedOrder = orders
-                .stream()
-                .filter(order -> order.getNumber().equals(number))
-                .collect(Collectors.toList()).get(0);
+        throw new OrderDoesntExistException();
+    }
 
-        return searchedOrder;
+    public List<Order> getOrders() {
+        return orders;
     }
 }

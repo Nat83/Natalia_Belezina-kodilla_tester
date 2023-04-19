028cd9729a0318e503630cf6d6043fc9f71bc249
Natalia <betalka@yahoo.com>
2023-04-19 13:10:51 +0200

    Moduł 7 - 7.1. Zadanie: Ćwiczymy testy z parametrami / cd

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -2,42 +2,42 @@ package com.kodilla.parametrized_tests.homework;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserValidatorTestSuite {
 
-    private UserValidator validator = new UserValidator();
-//    @ParameterizedTest
-//    @CsvSource(value = {"test-123,true", "TEST.123,true", "-._,true", "AAAAAAAAaaaaaaaaaaaa5555555 ,true"})
-//    public void shouldReturnTrueForUserNamesWihPermittedSigns(String input, boolean expected) {
-//        assertEquals(expected, validator.validateUsername(input));
-//    }
-
-//    @ParameterizedTest
-//    @CsvSource(value = {"1@Nat,false", "40.Nat*,false", "<nat>_,false", "/nAT ,false", "{nat77},false"})
-//    public void shouldReturnFalseForUserNamesWihNoPermittedSigns(String input, boolean expected) {
-//        assertEquals(expected, validator.validateUsername(input));
-//    }
-
-//    @ParameterizedTest
-//    @CsvSource(value = {"1_,false", "9,false", ".a,false", "-0 ,false", "A,false"})
-//    public void shouldReturnFalseForUsersWihTooShortName(String input, boolean expected) {
-//        assertEquals(expected, validator.validateUsername(input));
-//    }
+    private UserValidator userValidator = new UserValidator();
 
     @ParameterizedTest
-    @CsvSource(value = {"test,4", " OtHEr ,5", "E V e n t,5", "null ,4", "A,1"})
-    public void shouldReturnTrueForEmailsWithPermittedSigns(String input, boolean expected) {
-        assertEquals(expected, validator.validateEmail(input));
+    @CsvSource(value = {"test-123", "TEST.123", "-._", "AAAAAAAAaaaaaa55555", "AAA", "aaa", "123", "---", "___", "..."})
+    public void shouldReturnTrueForUserNamesWihPermittedSigns(String input) {
+        assertTrue(userValidator.validateUsername(input));
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"test,4", " OtHEr ,5", "E V e n t,5", "null ,4", "A,1"})
-    public void shouldReturnFalseForEmailsWithNoPermittedSigns(String input, boolean expected) {
-        assertEquals(expected, validator.validateEmail(input));
+    @CsvSource(value = {"1@Nat", "40.Nat*", "<nat>", "/nAT", "{nat77}"})
+    public void shouldReturnFalseForUserNamesWihNoPermittedSigns(String input) {
+        assertFalse(userValidator.validateUsername(input));
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"1_", "9", ".a", "-0", "A", "55", "Ac", "AA", "aa"})
+    public void shouldReturnFalseForUsersWihTooShortName(String input) {
+        assertFalse(userValidator.validateUsername(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"nat_b@gsoft.com", "NAT-B@g-soft.pl", "66-aa_Nat.aa-44_7@gSOFT.com", "_-_.-_-@--.pl",
+            "AAA.AAA@AAA.c", "aaa.aaa@aaa.comcom", "66-33.77_88@99-11.99-11.GB"})
+    public void shouldReturnTrueForEmailsWithCorrectFormat(String input) {
+        assertTrue(userValidator.validateEmail(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"nat_bgsoft.com", "NAT-B@g-softpl", "66-aa_Nat.aa-44_7@gSOFT_SOFT.com", "_-_.-_-@-_-.pl",
+            "M@LPA@AAA.c", "aaa.aaa@aaa.comcomm", "66+33.77=88@99-11.99-11.GB"})
+    public void shouldReturnFalseForEmailsWithIncorrectFormat(String input) {
+        assertFalse(userValidator.validateEmail(input));
+    }
 }

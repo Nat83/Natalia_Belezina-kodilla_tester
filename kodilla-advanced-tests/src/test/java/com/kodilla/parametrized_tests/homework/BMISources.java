3113544ed4c4a5478a957adb1dde352607587c79
Natalia <betalka@yahoo.com>
2023-04-20 14:07:00 +0200

    Moduł 7 - 7.3. Zadanie: kalkulator BMI

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/BMISources.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/BMISources.java
@@ -8,10 +8,14 @@ public class BMISources {
     static Stream<Arguments> providePersonsForBMITest() {
         return Stream.of(
                 Arguments.of(new Person(1.62, 67), "Overweight"),
-                Arguments.of(new Person(1.42, 100), "Obese Class IV (Morbidly Obese)"),
-                Arguments.of(new Person(1.50, 95), "Obese Class III (Very severely obese)"),
-                Arguments.of(new Person(1.47, 80), "Obese Class II (Severely obese)"),
-                Arguments.of(new Person(1.60, 80), "Obese Class I (Moderately obese)"),
+                Arguments.of(new Person(1.42, 100),
+                        "Obese Class IV (Morbidly Obese)"),
+                Arguments.of(new Person(1.50, 95),
+                        "Obese Class III (Very severely obese)"),
+                Arguments.of(new Person(1.47, 80),
+                        "Obese Class II (Severely obese)"),
+                Arguments.of(new Person(1.60, 80),
+                        "Obese Class I (Moderately obese)"),
                 Arguments.of(new Person(1.60, 38), "Very severely underweight"),
                 Arguments.of(new Person(1.70, 45), "Severely underweight"),
                 Arguments.of(new Person(1.80, 60), "Normal (healthy weight)"),
@@ -20,5 +24,4 @@ public class BMISources {
                 Arguments.of(new Person(1.70, 180), "Obese Class VI (Hyper Obese)")
         );
     }
-
 }

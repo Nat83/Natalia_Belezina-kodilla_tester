5b2c33f0a4d99e9c87e48d6526244f3b0b95dd5d
Natalia <betalka@yahoo.com>
2023-05-14 13:31:59 +0200

    Modul 10 - 10.4 Zadanie: ćwiczenia testów API

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest.java
@@ -14,37 +14,63 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BookControllerTest {
 
 
-//    @Test
-//    void shouldFetchBooks() {
-//        //given
-//        BookService bookServiceMock = Mockito.mock(BookService.class);
-//        BookController bookController = new BookController(bookServiceMock);
-//        List<BookDto> booksList = new ArrayList<>();
-//        booksList.add(new BookDto("Title 1", "Author 1"));
-//        booksList.add(new BookDto("Title 2", "Author 2"));
-//        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
-//
-//        //when
-//        List<BookDto> result = bookController.getBooks();
-//
-//        //then
-//        assertThat(result).hasSize(2);
-//    }
+    @Test
+    void shouldFetchBooks() {
+        //given
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(new BookDto("Title 1", "Author 1"));
+        booksList.add(new BookDto("Title 2", "Author 2"));
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
 
-//    @Test
-//    void shouldAddBook() {
-//        //given
-//        BookService bookServiceMock = Mockito.mock(BookService.class);
-//        BookController bookController = new BookController(bookServiceMock);
-//        List<BookDto> booksList = new ArrayList<>();
-//        BookDto addedBook = (new BookDto("Title 1", "Author 1"));
-//
-//
-//        //when
-//        bookController.addBook(addedBook);
-//        Mockito.verify(bookServiceMock.addBook(addedBook));
-//
-//        //then / nie dziala:(
-//        Assertions.assertEquals(1, bookController.getBooks().size());
-//    }
+        //when
+        List<BookDto> result = bookController.getBooks();
+
+        //then
+        assertThat(result).hasSize(2);
+    }
+
+    @Test
+    void canAddBook() {
+        //given
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        List<BookDto> booksList = new ArrayList<>();
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+        BookDto addedBook = (new BookDto("Title 1", "Author 1"));
+        //when
+        bookController.addBook(addedBook);
+        //then
+        Mockito.verify(bookServiceMock, Mockito.times(1)).addBook(addedBook);
+    }
+
+    @Test
+    void canAddBookWithTheSameDataMultipleTimes() {
+        //given
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        BookDto addedBook = (new BookDto("Title 1", "Author 1"));
+        //when
+        bookController.addBook(addedBook);
+        bookController.addBook(addedBook);
+        bookController.addBook(addedBook);
+        //then
+        Mockito.verify(bookServiceMock, Mockito.times(3)).addBook(addedBook);
+
+    }
+    @Test
+    void canAddDifferentBooks() {
+        //given
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        BookDto addedBook = (new BookDto("Title 1", "Author 1"));
+        BookDto addedBook2 = (new BookDto("Title 2", "Author 2"));
+        //when
+        bookController.addBook(addedBook);
+        bookController.addBook(addedBook2);
+        //then
+        Mockito.verify(bookServiceMock, Mockito.times(1)).addBook(addedBook);
+        Mockito.verify(bookServiceMock, Mockito.times(1)).addBook(addedBook2);
+    }
 }

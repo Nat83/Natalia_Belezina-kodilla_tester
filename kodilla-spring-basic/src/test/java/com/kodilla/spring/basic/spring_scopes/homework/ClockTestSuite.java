746b022c5459bce30b5b6cd3267fa0138604df76
Natalia <betalka@yahoo.com>
2023-05-14 12:19:42 +0200

    Modul 9 - 9.4. Adnotacja @Bean

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.basic.spring_scopes.homework;
 
-import com.kodilla.spring.basic.spring_scopes.Task;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,18 +7,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class ClockTestSuite {
 
     @Test
-    public void shouldCreateDifferentTimes() {
+    public void shouldCreateDifferentMultipleClockBeans() {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Clock firstBean = context.getBean(Clock.class);
-        Clock secondBean = context.getBean(Clock.class);
-        Clock thirdBean = context.getBean(Clock.class);
-        Assertions.assertNotEquals(firstBean, secondBean);
-        Assertions.assertNotEquals(secondBean, thirdBean);
-        Assertions.assertNotEquals(firstBean, thirdBean);
+        Clock clockBean1 = context.getBean(Clock.class);
+        Clock clockBean2 = context.getBean(Clock.class);
+        Clock clockBean3 = context.getBean(Clock.class);
+        Assertions.assertNotEquals(clockBean1, clockBean2);
+        Assertions.assertNotEquals(clockBean2, clockBean3);
+        Assertions.assertNotEquals(clockBean1, clockBean3);
     }
-
 }

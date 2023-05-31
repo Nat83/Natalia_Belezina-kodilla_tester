aa8bfdae496165d5a06de78848d92e9c5090feee
Natalia <betalka@yahoo.com>
2023-05-31 18:57:14 +0200

    Modul 13 - 13.4. 13.4. Wyszukiwanie elementów strony za pomocą XPath*

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,37 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AllegroTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\betal\\Desktop\\bootcampKodilla\\selenium-drivers\\" +
+                "Chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://allegro.pl/");
+
+        WebElement acceptCookieButton = driver.findElement
+                (By.xpath("//button[@data-role=\"accept-consent\"]"));
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOf(acceptCookieButton));
+        acceptCookieButton.click();
+
+        WebElement searchInput = driver.findElement(By.xpath("//*[@placeholder=\"czego szukasz?\"]"));
+        searchInput.sendKeys("Mavic mini");
+
+        WebElement typeCombo = driver.findElement
+                (By.xpath("//select[contains(@aria-label, \"Kategoria\")]"));
+        Select typeSelect = new Select(typeCombo);
+        typeSelect.selectByVisibleText("Elektronika");
+
+        WebElement submitButton = driver.findElement(By.xpath("//button[@type=\"submit\"]"));
+        submitButton.submit();
+    }
+}

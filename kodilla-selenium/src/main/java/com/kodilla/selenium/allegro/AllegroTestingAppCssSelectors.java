9f81cf640318507f927ad03a44b89bc1389a13b4
Natalia <betalka@yahoo.com>
2023-06-05 19:37:57 +0200

    Modul 13 - 13.5 Zadanie: Ćwiczymy selektory CSS

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCssSelectors.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCssSelectors.java
@@ -12,35 +12,65 @@ import java.util.List;
 
 public class AllegroTestingAppCssSelectors {
     public static void main(String[] args) {
+
+        //ALLEGRO
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\betal\\Desktop\\bootcampKodilla\\selenium-drivers\\" +
+//                "Chrome\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://allegro.pl/");
+//
+//        WebElement acceptCookieButton = driver.findElement
+//                (By.cssSelector("[data-role=accept-consent]"));
+//        WebDriverWait wait = new WebDriverWait(driver, 20);
+//        wait.until(ExpectedConditions.visibilityOf(acceptCookieButton));
+//        acceptCookieButton.click();
+//
+//        WebElement searchInput = driver.findElement(By.name("string"));
+//        searchInput.sendKeys("Mavic mini");
+//
+//        WebElement typeCombo = driver.findElement
+//                (By.cssSelector("select[aria-label*='Kategoria']"));
+//        Select typeSelect = new Select(typeCombo);
+//        typeSelect.selectByVisibleText("Elektronika");
+//
+//        WebElement submitButton = driver.findElement(By.cssSelector("[data-role=search-button]"));
+//        submitButton.submit();
+//
+//        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("section > article")));
+//
+//        WebElement confirmYouAreNotRobotButton = driver.findElement(By.id("captcha_display_button"));
+//        confirmYouAreNotRobotButton.submit();
+//
+//        List<WebElement> listOfArticles = driver.findElements(By.cssSelector("section > article"));
+//        listOfArticles.stream()
+//                .forEach(article -> System.out.println(article.getText()));
+
+        //EBAY
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\betal\\Desktop\\bootcampKodilla\\selenium-drivers\\" +
                 "Chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://allegro.pl/");
+        driver.get("https://www.ebay.pl/");
 
-        WebElement acceptCookieButton = driver.findElement
-                (By.cssSelector("[data-role=accept-consent]"));
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.visibilityOf(acceptCookieButton));
-        acceptCookieButton.click();
-
-        WebElement searchInput = driver.findElement(By.name("string"));
+        WebElement searchInput = driver.findElement(By.cssSelector(".ui-autocomplete-input"));
         searchInput.sendKeys("Mavic mini");
 
         WebElement typeCombo = driver.findElement
-                (By.cssSelector("select[aria-label*='Kategoria']"));
+                (By.name("_sacat"));
+        typeCombo.click();
         Select typeSelect = new Select(typeCombo);
-        typeSelect.selectByVisibleText("Elektronika");
+        typeSelect.selectByVisibleText("Fotografia i Kamery");
 
-        WebElement submitButton = driver.findElement(By.cssSelector("[data-role=search-button]"));
-        submitButton.submit();
-        
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("section > article")));
+        WebElement searchButton = driver.findElement(By.cssSelector("[type=submit]"));
+        searchButton.submit();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement productBoxes = driver.findElement(By.cssSelector("div>ul>li[data-viewport*='{\"trackableId']"));
+        wait.until(ExpectedConditions.visibilityOfAllElements(productBoxes));
 
-        WebElement confirmYouAreNotRobotButton = driver.findElement(By.id("captcha_display_button"));
-        confirmYouAreNotRobotButton.submit();
 
-        List<WebElement> listOfArticles = driver.findElements(By.cssSelector("section > article"));
-        listOfArticles.stream()
-                .forEach(article -> System.out.println(article.getText()));
+        List<WebElement> listOfProducts = driver.findElements
+                (By.cssSelector("div>ul>li[data-viewport*='{\"trackableId']"));
+        listOfProducts.stream()
+                .forEach(product -> System.out.println(product.findElement
+                        (By.cssSelector("span[role=\"heading\"]")).getText()));
     }
 }
